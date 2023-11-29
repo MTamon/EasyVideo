86b9acbddc3e051a307bee236a07ef456ae5dfd2
mago-deshi <31021843+MTamon@users.noreply.github.com>
2023-11-29 14:27:21 +0900

    Initial commit

---
+++ project/__init__.py
@@ -0,0 +1,24 @@
+"""Initialize published package."""
+
+# TODO: import modules here
+# example: from .src import module1, module2, module3
+
+############## EDIT THESE INFORMATION ###############
+AUTHOR = "John Smith"
+EMAIL = "john.smith@email.com"
+YEAR = "2023"
+GIT_URL = "https://github.com/john-smith/project.git"
+VERSION = "0.0.0"
+LICENCE = "MIT License"
+#####################################################
+
+__copyright__ = f"Copyright (C) {YEAR} {AUTHOR}"
+__version__ = VERSION
+__license__ = LICENCE
+__author__ = AUTHOR
+__author_email__ = EMAIL
+__url__ = GIT_URL
+
+# TODO: add modules here
+# example: __all__ = ["module1", "module2", "module3"]
+__all__ = []
