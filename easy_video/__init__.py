5fb2a4e02a0851f293c7c8af295705bcdc72d07c
MTamon <mtamon.0044@gmail.com>
2023-11-29 16:14:47 +0900

    [Fix] change project name for uploading pypi

--- easy_video/__init__.py
+++ easy_video/__init__.py
@@ -7,7 +7,7 @@ AUTHOR = "Tamon Mikawa"
 EMAIL = "mtamon.engineering@gmail.com"
 YEAR = "2023"
 GIT_URL = "https://github.com/MTamon/VideoTools.git"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 LICENCE = "MIT License"
 #####################################################
 
